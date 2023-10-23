d18cb4d6e199d075bf1e522a6e6417acbf6612f4
Jeremias Nunez <jeremias2@gmail.com>
2023-10-23 02:43:18 -0400

    feat:added shadcn components

---
+++ src/lib/ui/components/alert/alert.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { cn } from "$lib/common/utils/shadcn-utility";
+	import type { HTMLAttributes } from "svelte/elements";
+	import { alertVariants, type Variant } from ".";
+
+	type $$Props = HTMLAttributes<HTMLDivElement> & {
+		variant?: Variant;
+	};
+
+	let className: $$Props["class"] = undefined;
+	export let variant: $$Props["variant"] = "default";
+	export { className as class };
+</script>
+
+<div
+	class={cn(alertVariants({ variant }), className)}
+	{...$$restProps}
+	role="alert"
+>
+	<slot />
+</div>
---
+++ src/lib/ui/components/form/form-label.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Label as LabelPrimitive } from "bits-ui";
+	import { getFormField } from "formsnap";
+	import { cn } from "$lib/common/utils/shadcn-utility";
+	import { Label } from "$lib/ui/components/ui/label";
+
+	type $$Props = LabelPrimitive.Props;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+
+	const { errors, ids } = getFormField();
+</script>
+
+<Label
+	for={ids.input}
+	class={cn($errors && "text-destructive", className)}
+	{...$$restProps}
+>
+	<slot />
+</Label>
