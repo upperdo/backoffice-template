d5176ecab14c2a2ca3323d1b5d5d92eab2647426
Jeremias Nunez <jeremias2@gmail.com>
2023-10-25 00:07:00 -0400

    feat: StorageService tested

--- src/routes/(backoffice)/dashboard/+page.svelte
+++ src/routes/(backoffice)/dashboard/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-    import { DebugObject } from "$lib/ui/widgets"
     import DashboardPage from "$lib/features/dashboard/widgets/dashboard-page.svelte";
 
     export let data;
 
-    $:({ leads } = data);
+    $:({ leads, files } = data);
 </script>
-
 <DashboardPage />
